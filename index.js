2a68bbe29b13cb8dccb4f57327ab4b58c8c2d987
Telent1 <1418347552@qq.com>
2017-09-18 15:35:10 +0800

    基本完成功能

--- index.js
+++ index.js
@@ -1,22 +1,162 @@
+var input = ''
+var previousInput = ''
 function functionClick(params) {
-  console.log(params)
-  // var input = ''
-  // var previousInput = '',
-  // var calcInput = document.getElementById('calcInput').innerHTML
-  // var result = document.getElementById('result').innerHTML
-  // switch(params) {
-  //   case 'ac':
-  //   document.getElementById('calcInput').innerHTML = '0'
-  //   document.getElementById('result').innerHTML = '0'
-  //   previousInput = ''
-  //   break;
-  //   case '/':
-  //   if (previousInput) {
-  //     if (previousInput != '/'&& previousInput != '*' && previousInput != '-' && previousInput != '+') {
-  //       document.getElementById('calcInput').innerHTML = calcInput + '/'
-  //     } else {
-  //       calcInput = 
-  //     }
-  //   }
-  // }
+  console.log('params', params)
+  var calcInput = document.getElementById('calcInput').innerHTML
+  var result = document.getElementById('result').innerHTML
+  if (params == '/' || params == '*' || params == '-' || params == '+') {
+    calc()
+    function calc() {
+      if (calcInput.indexOf('/') != -1) {
+        let arr = calcInput.split('/')
+        if (Number(arr[1]) == 0) {
+          result = '除数不能为0'
+        } else {
+          result = Number(arr[0]) / Number(arr[1])
+        }
+      } else if (calcInput.indexOf('*') != -1) {
+        let arr = calcInput.split('*')
+        result = Number(arr[0]) * Number(arr[1])
+      } else if (calcInput.indexOf('-') != -1) {
+        let arr = calcInput.split('-')
+        result = Number(arr[0]) - Number(arr[1])
+      } else if (calcInput.indexOf('+') != -1) {
+        let arr = calcInput.split('+')
+        result = Number(arr[0]) + Number(arr[1])
+      }
+      result = result.toString()
+      result = result.substring(0, 9)
+      document.getElementById('result').innerHTML = result
+      calcInput = document.getElementById('calcInput').innerHTML
+      result = document.getElementById('result').innerHTML
+    }
+  }
+  switch (params) {
+    case 'ac':
+      document.getElementById('calcInput').innerHTML = '0'
+      document.getElementById('result').innerHTML = '0'
+      previousInput = ''
+      break;
+    case '/':
+      if (previousInput != '') {
+        if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+          if (result == '0') {
+            document.getElementById('calcInput').innerHTML = calcInput + '/'
+          } else {
+            document.getElementById('calcInput').innerHTML = result + '/'
+          }
+        } else {
+          let len = calcInput.length
+          calcInput = calcInput.substring(0, len - 1)
+          document.getElementById('calcInput').innerHTML = calcInput + '/'
+        }
+      } else {
+        document.getElementById('calcInput').innerHTML = calcInput + '/'
+      }
+      break;
+    case '*':
+      if (previousInput) {
+        if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+          if (result == '0') {
+            document.getElementById('calcInput').innerHTML = calcInput + '*'
+          } else {
+            document.getElementById('calcInput').innerHTML = result + '*'
+          }
+        } else {
+          let len = calcInput.length
+          calcInput = calcInput.substring(0, len - 1)
+          document.getElementById('calcInput').innerHTML = calcInput + '*'
+        }
+      } else {
+        document.getElementById('calcInput').innerHTML = calcInput + '*'
+      }
+      break;
+    case '-':
+      if (previousInput) {
+        if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+          if (result == '0') {
+            document.getElementById('calcInput').innerHTML = calcInput + '-'
+          } else {
+            document.getElementById('calcInput').innerHTML = result + '-'
+          }
+        } else {
+          let len = calcInput.length
+          calcInput = calcInput.substring(0, len - 1)
+          document.getElementById('calcInput').innerHTML = calcInput + '-'
+        }
+      } else {
+        document.getElementById('calcInput').innerHTML = calcInput + '-'
+      }
+      break;
+    case '+':
+      if (previousInput) {
+        if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+          if (result == '0') {
+            document.getElementById('calcInput').innerHTML = calcInput + '+'
+          } else {
+            document.getElementById('calcInput').innerHTML = result + '+'
+          }
+        } else {
+          let len = calcInput.length
+          calcInput = calcInput.substring(0, len - 1)
+          document.getElementById('calcInput').innerHTML = calcInput + '+'
+        }
+      } else {
+        document.getElementById('calcInput').innerHTML = calcInput + '+'
+      }
+      break;
+      case '=':
+        if (calcInput.indexOf('/') != -1) {
+          let arr = calcInput.split('/')
+          if (Number(arr[1]) == '0') {
+            result = '除数不能为0'
+          } else {
+            result = Number(arr[0]) / Number(arr[1])
+          }
+        } else if (calcInput.indexOf('*') != -1) {
+          let arr = calcInput.split('*')
+          result = Number(arr[0]) * Number(arr[1])
+        } else if (calcInput.indexOf('-') != -1) {
+          let arr = calcInput.split('-')
+          result = Number(arr[0]) - Number(arr[1])
+        } else if (calcInput.indexOf('+') != -1) {
+          let arr = calcInput.split('+')
+          result = Number(arr[0]) + Number(arr[1])
+        }
+        result = result.toString()
+        result = result.substring(0, 9)
+        document.getElementById('result').innerHTML = result
+      break;
+      case '.':
+      console.log('.')
+      if (previousInput) {
+        if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+          document.getElementById('calcInput').innerHTML = calcInput + '.'
+        } else {
+          document.getElementById('calcInput').innerHTML = calcInput + '0.'
+        }
+      } else {
+        document.getElementById('calcInput').innerHTML = calcInput + '0.'
+      }
+      break;
+    default:
+      if (previousInput) {
+        if (previousInput != '0') {
+          document.getElementById('calcInput').innerHTML = calcInput + params
+        } else {
+          let len = calcInput.length
+          calcInput = calcInput.substring(0, len - 1)
+          document.getElementById('calcInput').innerHTML = calcInput + params
+        }
+      } else {
+        let len = calcInput.length
+        calcInput = calcInput.substring(0, len - 1)
+        document.getElementById('calcInput').innerHTML = calcInput + params
+      }
+      break;
+  }
+  if (params != 'ac') {
+    previousInput = params
+  }
+  console.log('pre', previousInput)
 }
