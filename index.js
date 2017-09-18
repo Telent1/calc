d62f1fc4cfb491eadfb2b5d5cff5d578828da9bf
Telent1 <1418347552@qq.com>
2017-09-18 17:25:49 +0800

    修复一个输入等于号后出现的bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+// 键盘输入监听事件
 window.addEventListener('keypress',function(event) {
-  console.log(event)
   if (event.keyCode >= 48 && event.keyCode <= 57) {
     functionClick(event.key.toString())
   } else if (event.keyCode == 47 || event.keyCode == 42 || event.keyCode == 45 || event.keyCode == 43 || event.keyCode == 46) {
@@ -8,38 +8,46 @@ window.addEventListener('keypress',function(event) {
     functionClick('=')
   }
 })
-
+// 对输入的数字进行判断并进行计算
 var input = ''
 var previousInput = ''
 function functionClick(params) {
   console.log('params', params)
   var calcInput = document.getElementById('calcInput').innerHTML
   var result = document.getElementById('result').innerHTML
-  if (params == '/' || params == '*' || params == '-' || params == '+') {
-    calc()
-    function calc() {
-      if (calcInput.indexOf('/') != -1) {
-        let arr = calcInput.split('/')
-        if (Number(arr[1]) == 0) {
-          result = '除数不能为0'
-        } else {
-          result = Number(arr[0]) / Number(arr[1])
+  if ((params == '/' || params == '*' || params == '-' || params == '+')) {
+    if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
+      calc()
+      function calc() {
+        console.log('calc')
+        if (calcInput.indexOf('/') != -1) {
+          let arr = calcInput.split('/')
+          if (Number(arr[1]) == 0) {
+            result = '除数不能为0'
+          } else {
+            result = Number(arr[0]) / Number(arr[1])
+          }
+        } else if (calcInput.indexOf('*') != -1) {
+          let arr = calcInput.split('*')
+          result = Number(arr[0]) * Number(arr[1])
+        } else if (calcInput.indexOf('-') != -1) {
+          let arr = calcInput.split('-')
+          result = Number(arr[0]) - Number(arr[1])
+        } else if (calcInput.indexOf('+') != -1) {
+          let arr = calcInput.split('+')
+          result = Number(arr[0]) + Number(arr[1])
         }
-      } else if (calcInput.indexOf('*') != -1) {
-        let arr = calcInput.split('*')
-        result = Number(arr[0]) * Number(arr[1])
-      } else if (calcInput.indexOf('-') != -1) {
-        let arr = calcInput.split('-')
-        result = Number(arr[0]) - Number(arr[1])
-      } else if (calcInput.indexOf('+') != -1) {
-        let arr = calcInput.split('+')
-        result = Number(arr[0]) + Number(arr[1])
+        result = result.toString()
+        result = result.substring(0, 9)
+        document.getElementById('result').innerHTML = result
+        calcInput = document.getElementById('calcInput').innerHTML
+        result = document.getElementById('result').innerHTML
       }
-      result = result.toString()
-      result = result.substring(0, 9)
-      document.getElementById('result').innerHTML = result
-      calcInput = document.getElementById('calcInput').innerHTML
-      result = document.getElementById('result').innerHTML
+    } else {
+      let len = calcInput.length
+      console.log('len', len)
+      calcInput = calcInput.substring(0, len)
+      document.getElementById('calcInput').innerHTML = calcInput + params
     }
   }
   switch (params) {
@@ -51,7 +59,7 @@ function functionClick(params) {
     case '/':
       if (previousInput != '') {
         if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
-          if (result == '0') {
+          if ((result == '0' || calcInput != '0') && previousInput != '=') {
             document.getElementById('calcInput').innerHTML = calcInput + '/'
           } else {
             document.getElementById('calcInput').innerHTML = result + '/'
@@ -68,7 +76,7 @@ function functionClick(params) {
     case '*':
       if (previousInput) {
         if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
-          if (result == '0') {
+          if ((result == '0' || calcInput != '0') && previousInput != '=') {
             document.getElementById('calcInput').innerHTML = calcInput + '*'
           } else {
             document.getElementById('calcInput').innerHTML = result + '*'
@@ -85,7 +93,7 @@ function functionClick(params) {
     case '-':
       if (previousInput) {
         if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
-          if (result == '0') {
+          if ((result == '0' || calcInput != '0') && previousInput != '=') {
             document.getElementById('calcInput').innerHTML = calcInput + '-'
           } else {
             document.getElementById('calcInput').innerHTML = result + '-'
@@ -102,7 +110,7 @@ function functionClick(params) {
     case '+':
       if (previousInput) {
         if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
-          if (result == '0') {
+          if ((result == '0' || calcInput != '0') && previousInput != '=') {
             document.getElementById('calcInput').innerHTML = calcInput + '+'
           } else {
             document.getElementById('calcInput').innerHTML = result + '+'
@@ -137,6 +145,7 @@ function functionClick(params) {
         result = result.toString()
         result = result.substring(0, 9)
         document.getElementById('result').innerHTML = result
+        // document.getElementById('calcInput').innerHTML = result
       break;
       case '.':
       console.log('.')
@@ -151,7 +160,7 @@ function functionClick(params) {
       }
       break;
     default:
-      if (previousInput) {
+      if (previousInput != '=' && previousInput != '') {
         if (previousInput != '0') {
           document.getElementById('calcInput').innerHTML = calcInput + params
         } else {
@@ -159,7 +168,9 @@ function functionClick(params) {
           calcInput = calcInput.substring(0, len - 1)
           document.getElementById('calcInput').innerHTML = calcInput + params
         }
-      } else {
+      } else if (previousInput == '=') {
+        document.getElementById('calcInput').innerHTML = params
+      }else {
         let len = calcInput.length
         calcInput = calcInput.substring(0, len - 1)
         document.getElementById('calcInput').innerHTML = calcInput + params
