29465a826d86ca0d8514dade6339e628bb99f3a9
Telent1 <1418347552@qq.com>
2017-09-18 14:22:23 +0800

    增加点击函数

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// function functionClick(params) {
-//   console.log(params)
-//   var input = ''
-//   var previousInput = '',
-//   var calcInput = document.getElementById('calcInput').innerHTML
-//   var result = document.getElementById('result').innerHTML
-//   switch(params) {
-//     case 'ac':
-//     document.getElementById('calcInput').innerHTML = '0'
-//     document.getElementById('result').innerHTML = '0'
-//     previousInput = ''
-//     break;
-//     case '/':
-//     if (previousInput) {
-//       if (previousInput != '/'&& previousInput != '*' && previousInput != '-' && previousInput != '+') {
-//         document.getElementById('calcInput').innerHTML = calcInput + '/'
-//       } else {
-//         calcInput = 
-//       }
-//     }
-//   }
-// }
+function functionClick(params) {
+  console.log(params)
+  // var input = ''
+  // var previousInput = '',
+  // var calcInput = document.getElementById('calcInput').innerHTML
+  // var result = document.getElementById('result').innerHTML
+  // switch(params) {
+  //   case 'ac':
+  //   document.getElementById('calcInput').innerHTML = '0'
+  //   document.getElementById('result').innerHTML = '0'
+  //   previousInput = ''
+  //   break;
+  //   case '/':
+  //   if (previousInput) {
+  //     if (previousInput != '/'&& previousInput != '*' && previousInput != '-' && previousInput != '+') {
+  //       document.getElementById('calcInput').innerHTML = calcInput + '/'
+  //     } else {
+  //       calcInput = 
+  //     }
+  //   }
+  // }
+}
