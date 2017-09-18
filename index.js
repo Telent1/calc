145d38103267461beff379d00635e58ebc634256
Telent1 <1418347552@qq.com>
2017-09-18 18:38:00 +0800

    修复不知道什么小虫虫

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ function functionClick(params) {
   // console.log('params', params)
   var calcInput = document.getElementById('calcInput').innerHTML
   var result = document.getElementById('result').innerHTML
-  if ((params == '/' || params == '*' || params == '-' || params == '+') && (calcInput.indexOf('/') != -1 || calcInput.indexOf('*') != -1 ||calcInput.indexOf('-') != -1 || calcInput.indexOf('+') != -1)) {
+  let index = -1
+  let index1 = calcInput.indexOf('/')
+  let index2 = calcInput.indexOf('*')
+  let index3 = calcInput.indexOf('-')
+  let index4 = calcInput.indexOf('+')
+  if (index1 != -1) {
+    index = index1
+  } else if (index2 != -1) {
+    index = index2
+  } else if (index3 != -1) {
+    index = index3
+  } else if (index4 != -1) {
+    index = index4
+  }
+  // console.log(index, 'index')
+  if ((params == '/' || params == '*' || params == '-' || params == '+') && index != -1 && index != (calcInput.length - 1)) {
     if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
       calc()
       function calc() {
@@ -38,7 +53,9 @@ function functionClick(params) {
           result = Number(arr[0]) + Number(arr[1])
         }
         result = result.toString()
-        result = result.substring(0, 9)
+        if (result.length > 9) {
+          result = result.substring(0, 9)
+        }
         document.getElementById('result').innerHTML = result
         document.getElementById('calcInput').innerHTML = result + params
         calcInput = document.getElementById('calcInput').innerHTML
@@ -47,11 +64,13 @@ function functionClick(params) {
     } else {
       let len = calcInput.length
       // console.log('len', len)
-      calcInput = calcInput.substring(0, len)
+      calcInput = calcInput.substring(0, len - 1)
       document.getElementById('calcInput').innerHTML = calcInput + params
     }
+    previousInput = params
     return
   }
+  // console.log(previousInput)
   switch (params) {
     case 'ac':
       document.getElementById('calcInput').innerHTML = '0'
@@ -59,7 +78,7 @@ function functionClick(params) {
       previousInput = ''
       break;
     case '/':
-      if (previousInput != '') {
+      if (previousInput) {
         if (previousInput != '/' && previousInput != '*' && previousInput != '-' && previousInput != '+') {
           if ((result == '0' || calcInput != '0') && previousInput != '=') {
             document.getElementById('calcInput').innerHTML = calcInput + '/'
